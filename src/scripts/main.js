95f6c878a917e47ff7a79553551a42288a911700
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2022-07-20 17:39:56 +0100

    UPD - Refactor employee avatar to fallback via client JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,9 @@
 import linkCopy from './components/link-copy.js';
 import colorCopy from './components/color-copy.js';
+import employee from './components/employee.js';
 import sidebar from './layout/sidebar.js';
 
 linkCopy();
 colorCopy();
+employee();
 sidebar();
