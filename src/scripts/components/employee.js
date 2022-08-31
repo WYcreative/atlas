7a59a2fa6d7b045e76437a3b6f6b368e38c20aff
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2022-08-31 17:07:55 +0100

    FIX - Employee avatar sometimes not having fallback applied

--- src/scripts/components/employee.js
+++ src/scripts/components/employee.js
@@ -2,9 +2,17 @@ function employee() {
 	const images = document.querySelectorAll('.employee__img img');
 
 	for (const image of images) {
-		image.addEventListener('error', () => {
-			image.src = `https://wycreative.com/wycreative-assinaturas/team/random-${Math.ceil(Math.random() * 4)}.jpg`;
-		});
+		const fallbackSrc = `https://wycreative.com/wycreative-assinaturas/team/random-${Math.ceil(Math.random() * 4)}.jpg`;
+
+		if (image.complete === false) {
+			image.addEventListener('error', () => {
+				image.src = fallbackSrc;
+			}, {
+				once: true,
+			});
+		} else if (image.naturalWidth === 0) {
+			image.src = fallbackSrc;
+		}
 	}
 }
 
