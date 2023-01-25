c53d027a14846786079c2cc6f685ca0e7a631f03
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2023-01-25 10:22:18 +0000

    FIX - Test content not being fully watched

--- gulp/views.js
+++ gulp/views.js
@@ -1,7 +1,6 @@
 import {createRequire} from 'node:module';
 
 import config from '../config/index.js';
-import atlas from '../tests/atlas.js';
 import generateAtlas from '../scripts/index.js';
 
 
@@ -9,7 +8,9 @@ import generateAtlas from '../scripts/index.js';
 const pkg = createRequire(import.meta.url)('../package.json');
 
 
-function build(done) {
+async function build(done) {
+	const {default: atlas} = await import(`../tests/atlas.js?t=${Date.now()}`);
+
 	try {
 		generateAtlas({
 			package: pkg,
