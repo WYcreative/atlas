0896b01a78ced2c96323efc26f6bf3c33dd1e2e7
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2023-06-16 12:15:19 +0100

    UPD - Registry to be npm instead

--- scripts/dependencies.js
+++ scripts/dependencies.js
@@ -2,15 +2,8 @@ function generateDependencies(dependencies) {
 	const result = {};
 
 	for (const [name, version] of Object.entries(dependencies)) {
-		let url;
-
 		// REVIEW: Get this information from the packages/registries instead.
-		if (name.startsWith('@wycreative')) {
-			const unscopedPackage = name.split('/')[1];
-			url = `https://github.com/WYcreative/${unscopedPackage}/pkgs/npm/${unscopedPackage}`;
-		} else {
-			url = `https://www.npmjs.com/package/${name}`;
-		}
+		const url = `https://www.npmjs.com/package/${name}`;
 
 		result[name] = {
 			name,
